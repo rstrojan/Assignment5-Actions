5d754f093aa7f701a7c3d3f50fd3aaa9af38c4b7
Ryan Suter <rstrojan@gmail.com>
2020-02-28 16:53:00 -0800

    fixed stylings in tests

--- tests.py
+++ tests.py
@@ -1,7 +1,8 @@
 import unittest
 import task
 
-class TestCase(unittest.TestCase) :
+
+class TestCase(unittest.TestCase):
     def test1(self):
         expected = "success"
         self.assertEqual(expected, task.firstrun())
@@ -9,6 +10,7 @@ class TestCase(unittest.TestCase) :
     def test2(self):
         expected = "failure"
         self.assertEqual(expected, task.firstrun())
+
 
 if __name__ == '__main__':
     unittest.main()

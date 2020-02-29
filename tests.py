a6f7729cf3dea86af4714279f006c7a635a10e12
Ryan Suter <rstrojan@gmail.com>
2020-02-28 18:57:23 -0800

    radius to area - updated syntax 3

--- tests.py
+++ tests.py
@@ -20,9 +20,8 @@ class Test(TestCase):
     def test_time_delta(self):
         date1 = "12/14/1987"
         date2 = "12/14/1988"
-        result = task.time_delta(date1,date2)
+        result = task.time_delta(date1, date2)
         self.assertEqual(366, result)  # 88 was a leap year
-
 
 if __name__ == '__main__':
     unittest.main()

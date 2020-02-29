70e46caf049dbce62d97783e081c1209b90a96d9
Ryan Suter <rstrojan@gmail.com>
2020-02-28 17:29:52 -0800

    radius to area added

--- task.py
+++ task.py
@@ -1,2 +1,8 @@
+import math
+
 def firstrun():
     return "success"
+
+def Radius_to_Area(radius):
+    area = math.pi * math.sqrt(radius)
+    return area
--- tests.py
+++ tests.py
@@ -11,6 +11,10 @@ class TestCase(unittest.TestCase):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
 
+    def Radius_to_Area_test(self):
+        self.assertEqual(28.27, task.Radius_to_Area(3))
+        self.assertEqual(50.27, task.Radius_to_Area(4))
+
 
 if __name__ == '__main__':
     unittest.main()

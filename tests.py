406590af833ed84e4d469b56aff979f8efccb1e2
Ryan Suter <rstrojan@gmail.com>
2020-02-28 16:49:51 -0800

    fixed typo in test

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ class TestCase(unittest.TestCase) :
         expected = "success"
         self.assertEqual(expected, task.firstrun())
 
-    def test2(selfs):
+    def test2(self):
         expected = "failure"
         self.assertEqual(expected, task.firstrun())
 

0b24cabaabccea3d8c6f23137b5bd844af681804
Ryan Suter <rstrojan@gmail.com>
2020-02-28 17:52:54 -0800

    first_last added

--- task.py
+++ task.py
@@ -5,6 +5,13 @@ def firstrun():
     return "success"
 
 
-def Radius_to_Area(radius):
+def radius_to_area(radius):
     area = math.pi * math.sqrt(radius)
     return area
+
+
+def first_last(a_list):
+    temp_list = []
+    temp_list.append(a_list(0))
+    temp_list.append(a_list(-1))
+    return temp_list
--- tests.py
+++ tests.py
@@ -12,8 +12,23 @@ class TestCase(unittest.TestCase):
         self.assertNotEqual(expected, task.firstrun())
 
     def Radius_to_Area_test(self):
-        self.assertEqual(28.27, task.Radius_to_Area(3))
-        self.assertEqual(50.27, task.Radius_to_Area(4))
+        self.assertEqual(28.27, task.radius_to_area(3))
+        self.assertEqual(50.27, task.radius_to_area(4))
+
+    def first_last_test(self):
+        list_1 = [1, 2, 3]
+        result = task.first_last(list_1)
+        expected_1 = 1
+        expected_2 = 2
+        self.assertEqual(expected_1, result[0])
+        self.assertEqual(expected_2, result[1])
+
+        list_2 = [4, -1, 0, "Ryan"]
+        result = task.first_last(list_2)
+        expected_1 = 4
+        expected_2 = "Ryan"
+        self.assertEqual(expected_1, result[0])
+        self.assertEqual(expected_2, result[1])
 
 
 if __name__ == '__main__':

7c41880c59dd9d1452ff36dd032ccda01f1c9e7d
Ryan Suter <rstrojan@gmail.com>
2020-03-01 17:54:01 -0800

    removed date delta

--- task.py
+++ task.py
@@ -11,12 +11,6 @@ def radius_to_area(radius):
     return round(area, 2)
 
 
-def time_delta(date1, date2):
-    d1 = datetime.strptime(date1, "%m/%d/%Y")
-    d2 = datetime.strptime(date2, "%m/%d/%Y")
-    return abs(d2-d1).days
-
-
 def first_last(a_list):
     temp_arr = []
     temp_arr.append(a_list[0])
--- tests.py
+++ tests.py
@@ -17,12 +17,6 @@ class Test(TestCase):
         self.assertEqual(28.27, task.radius_to_area(3))
         self.assertEqual(50.27, task.radius_to_area(4))
 
-    def test_time_delta(self):
-        date1 = "12/14/1987"
-        date2 = "12/14/1988"
-        result = task.time_delta(date1, date2)
-        self.assertEqual(366, result)  # 88 was a leap year
-
     def test_first_last(self):
         my_arr = [1, 2, 3]
         result = task.first_last(my_arr)

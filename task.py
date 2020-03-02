95dabfe6e7ffb40c242ba515c5fa02ce7eccbaf6
Ryan Suter <rstrojan@gmail.com>
2020-03-01 17:55:03 -0800

    Date Detla updated

--- task.py
+++ task.py
@@ -16,3 +16,9 @@ def first_last(a_list):
     temp_arr.append(a_list[0])
     temp_arr.append(a_list[-1])
     return temp_arr
+
+
+def time_delta(date1, date2):
+   d1 = datetime.strptime(date1, "%m/%d/%Y")
+   d2 = datetime.strptime(date2, "%m/%d/%Y")
+   return abs(d2-d1).days
